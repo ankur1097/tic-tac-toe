a7f09aa8eb08d916afaafc77b235a2b2840c4b50
ankur1097 <70051898+ankur1097@users.noreply.github.com>
2020-08-22 14:20:56 +0530

    Delete notify.py

--- notify.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-from win10toast import Toastnotifier
-n=ToastNotifier()
-f=feedparser.parse("https://zeenews.india.com/rss/sports-news.xml")
-for news in f['item']:
-    n.show_toast(news['title'],news['summary'],duration=5)
